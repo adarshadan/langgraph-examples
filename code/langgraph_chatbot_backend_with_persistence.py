544c4f8dc13373291e5a804b670682bfb75330f2
Adarsha Dan <adarshadan@gmail.com>
2026-07-11 15:29:09 +0530

    Feat: Added chatbots with db persistence logic

---
+++ code/langgraph_chatbot_backend_with_persistence.py
@@ -0,0 +1,121 @@
+#Workflow to implement a simple chatbot using langgraph and streamlit
+#Package imports and load dot env
+import os,sqlite3,time
+from langgraph.graph import StateGraph
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_core.messages import BaseMessage,SystemMessage, HumanMessage
+from langgraph.graph import add_messages
+from langchain_groq import ChatGroq
+
+from typing import Annotated, List,TypedDict
+from dotenv import load_dotenv
+
+load_dotenv()
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Define the llm model for the chatbot
+
+llm = ChatGroq(model=os.getenv('GROQ_MODEL'),
+               api_key=os.getenv('GROQ_API_KEY'),
+               temperature=0.2)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#class to define the state parameters 
+
+class MessageState(TypedDict):
+
+    messages : Annotated[List[BaseMessage], add_messages]
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Method to invoke the llm 
+
+def chat_session(state:MessageState):
+
+    SYSTEM_PROMPT = SystemMessage(content="You are Chatty, a helpful AI assistant. " \
+    +"If ONLY asked who created or built you, say you were created by Adarsha Dan who is an Computer Science Engineer with 12+ years of IT experience in AI and Automation.")
+    
+    current_messages = [SYSTEM_PROMPT] + state['messages']
+
+    response = llm.invoke(current_messages)
+
+    return { 'messages': [response]}
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Method to generate label based on user conversation history
+
+def generate_label(input_message:str) -> str:
+
+    system_prompt = SystemMessage(content="Return ONLY the label text in 4-5 words. No quotes, no punctuation, no prefixes like 'Label:', no explanations." \
+    "If the message is just a greeting or filler with no real topic, respond with [_NO_LABEL_] instead of a label.")
+
+    user_prompt = HumanMessage(content=input_message)
+
+    prompt = [system_prompt,user_prompt]
+
+    response = llm.invoke(prompt).content.strip()
+
+    return '' if '_no_label_' in response.lower() else response
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Get all the unique threads
+
+def get_all_unique_threads():
+
+    cursor = conn.execute('SELECT thread_id, label from thread_label_mapping')
+    rows = cursor.fetchall()
+
+    label_map = {thread_id:label for thread_id,label in rows}
+
+    all_threads = {}
+    for checkpoint in checkpointer.list(None):
+        thread_id = checkpoint.config['configurable']['thread_id']
+        all_threads[thread_id] = label_map.get(thread_id,'')
+
+    return all_threads
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Method to insert the thread_id,label mapping into the SQLite db table
+
+def save_label(thread_id:str, label:str):
+
+    conn.execute('''INSERT INTO thread_label_mapping
+                 VALUES(?,?)
+                 ON CONFLICT(thread_id) 
+                 DO UPDATE 
+                 SET label = excluded.label''',(thread_id,label))
+    conn.commit()
+ 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Define the State graph, nodes and edges
+
+graph = StateGraph(MessageState)
+
+graph.add_node('chat_session',chat_session)
+
+graph.add_edge('__start__','chat_session')
+graph.add_edge('chat_session','__end__')
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Setup the SQLite db, checkpointer and compile the graph and generate the workflow graph object
+
+conn = sqlite3.connect(database='./chatbot.db',check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+conn.execute('''CREATE TABLE IF NOT EXISTS thread_label_mapping
+             (thread_id TEXT PRIMARY KEY,
+             label TEXT) ''')
+conn.commit()
+
+workflow = graph.compile(checkpointer=checkpointer)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Logic to test the backend implementation
+
+if __name__ == '__main__':
+
+    thread_id = 1
+    config = { 'configurable':{'thread_id': thread_id}}
+
+    stream_object = workflow.stream({'messages': [HumanMessage('How many inches in a feet?')]}, config=config, stream_mode='messages')
+
+    for message_chunk , metadata in stream_object:
+        
+        for char in message_chunk.content:
+            print(char, end='', flush=True) # No space between characters
+            time.sleep(0.01) # Shorter delay for characters
---
+++ code/langgraph_chatbot_frontend_with_persistence.py
@@ -0,0 +1,107 @@
+#import packages
+
+import streamlit as st
+from langchain_core.messages import HumanMessage
+from langgraph_chatbot_backend_with_persistence import workflow,generate_label,get_all_unique_threads,save_label
+import uuid
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Utility Methods
+
+def generate_thread_id():
+    return str(uuid.uuid4())
+
+def reset_chat():
+    temp_thread_id = ''
+    for thread_id, label in st.session_state.chat_threads.items():
+        if label == '':
+            temp_thread_id = thread_id
+            break
+
+    thread_id  = temp_thread_id or generate_thread_id()
+    st.session_state.thread_id = thread_id
+    add_thread(thread_id)
+    st.session_state.message_history = []
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state.chat_threads:
+        st.session_state.chat_threads[thread_id]=''
+
+
+def get_past_messages(thread_id):
+    try:
+        messages = workflow.get_state(config={'configurable':{'thread_id':thread_id}}).values['messages']
+    except KeyError:
+        messages  = [] #in case there are no messages typed in last session
+    return messages
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Session Setup
+
+if 'message_history' not in st.session_state:
+    st.session_state.message_history = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state.thread_id = generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state.chat_threads=get_all_unique_threads()
+
+add_thread(st.session_state.thread_id)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#UI page config, title, sidebar elements
+
+st.set_page_config(page_title="Chatty", page_icon="💬")
+st.title("💬 Chatty")
+st.sidebar.title('Chatty - Your Personal AI Assistant')
+if st.sidebar.button('➕ New Chat'):
+    reset_chat()
+st.sidebar.header('📝 Conversations')
+
+for thread_id in reversed(st.session_state.chat_threads):
+
+    label = st.session_state.chat_threads[thread_id] or '🚀 Current Conversation..'
+    if st.sidebar.button(label,key=thread_id):
+        st.session_state.thread_id = thread_id
+        temp_messages = get_past_messages(thread_id)
+
+        message_history =[]
+        for message in temp_messages:
+            if isinstance(message, HumanMessage):
+               role = 'user'
+            else:
+               role = 'assistant'
+            message_history.append({ 'role': role, 'content': message.content})
+        st.session_state.message_history = message_history
+            
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Display the past chat messages on the chat window
+
+for msg in st.session_state.message_history:
+    with st.chat_message(msg['role']):
+        st.text(msg['content'])      
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Get user input and stream responses on the UI
+
+config = {'configurable': {'thread_id': st.session_state.thread_id}}
+user_message = st.chat_input('Type here....')
+ai_message = ""
+
+if user_message:
+    with st.chat_message('user'):
+        st.text(user_message)
+    if not st.session_state.chat_threads[st.session_state.thread_id]:
+        st.session_state.chat_threads[st.session_state.thread_id] = generate_label(user_message)
+        save_label(st.session_state.thread_id, st.session_state.chat_threads[st.session_state.thread_id])
+
+    with st.chat_message('assistant'):
+        stream_object = workflow.stream({"messages": [HumanMessage(user_message)]},
+                                        config=config,
+                                        stream_mode='messages'
+                                        )
+        ai_message = st.write_stream( message_chunk.content for message_chunk, _ in stream_object)
+
+    st.session_state.message_history.append({'role': 'user', 'content': user_message})
+    st.session_state.message_history.append({'role': 'assistant', 'content': ai_message })
+    st.rerun()
